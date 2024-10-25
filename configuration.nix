5e15df34d47fb164ae245d95d49ea7598b322966
Harro <harro-forge@protonmail.com>
2024-10-25 18:35:38 +0200

    update

--- configuration.nix
+++ configuration.nix
@@ -14,6 +14,8 @@
   catppuccin.flavor = "mocha";
 
   # Bootloader.
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.efiSupport = true;
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "nodev";
   boot.loader.grub.useOSProber = true;
