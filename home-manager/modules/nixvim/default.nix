a28e40bdbe15fdee9de6da5c746be27cfe1dbbcb
Harro <harro-forge@protonmail.com>
2024-10-30 21:58:26 +0100

    refacto: modules

--- home-manager/modules/nixvim/default.nix
+++ home-manager/modules/nixvim/default.nix
@@ -7,47 +7,11 @@
     nixvim.enable = lib.mkEnableOption "nixvim";
   };
   imports = [
-    ./options/options.nix
-    ./options/clipboard.nix
+    ./options
 
-    ./plugins/global/neo-tree.nix
-    ./plugins/global/treesitter.nix
-    ./plugins/global/none-ls.nix
-    ./plugins/global/cmp.nix
-    ./plugins/global/luasnip.nix
-    ./plugins/global/lsp.nix
-    ./plugins/global/alpha.nix
-    ./plugins/global/floaterm.nix
-    ./plugins/global/flash.nix
-    # ./plugins/global/copilot.nix
-    ./plugins/global/plenary.nix
-    ./plugins/global/git.nix
-    ./plugins/global/image.nix
-    ./plugins/global/lualine.nix
-    ./plugins/global/neogen.nix
-    ./plugins/global/noice.nix
-    ./plugins/global/tmux-navigator.nix
-    ./plugins/global/surround.nix
-    ./plugins/global/telescope.nix
-    ./plugins/global/which-key.nix
-    ./plugins/global/oil.nix
-    ./plugins/global/otter.nix
+    ./plugins/global
 
-    ./plugins/languages/c.nix
-    ./plugins/languages/coq.nix
-    ./plugins/languages/css.nix
-    ./plugins/languages/docker.nix
-    ./plugins/languages/html.nix
-    ./plugins/languages/java.nix
-    ./plugins/languages/json.nix
-    ./plugins/languages/markdown.nix
-    ./plugins/languages/nix.nix
-    ./plugins/languages/obsidian.nix
-    ./plugins/languages/python.nix
-    ./plugins/languages/ts.nix
-    ./plugins/languages/xml.nix
-    ./plugins/languages/yaml.nix
-    ./plugins/languages/bash.nix
+    ./plugins/languages
   ];
 
   config = lib.mkIf config.nixvim.enable {
