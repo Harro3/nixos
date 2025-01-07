4004a3cc503d16886ccf7a88d38cd1bb167a9a65
Harro <harro-forge@protonmail.com>
2025-01-07 13:12:49 +0100

    fix: fixed bluetooth rfkill software block

--- hosts/harro-legion/default.nix
+++ hosts/harro-legion/default.nix
@@ -43,6 +43,18 @@
   hardware.bluetooth.powerOnBoot = true;
   services.openssh.enable = true;
 
+  systemd.services.unblock-bluetooth = {
+    description = "Unblock bluetooth via rfkill";
+    wantedBy = ["multi-user.target"];
+    serviceConfig = {
+      ExecStart = "${pkgs.util-linux}/bin/rfkill unblock bluetooth";
+      type = "oneshot";
+    };
+  };
+
+  systemd.services.unblock-bluetooth.enable = true;
+  boot.kernelParams = ["bluetooth.enable=1"];
+
   # Misc config
   catppuccin.flavor = "mocha";
   security.pam.services.hyprlock = {};
