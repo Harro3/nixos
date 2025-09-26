04634920e8001b66eed63ab23a6928e85ee7c480
Harro <harro-forge@protonmail.com>
2025-09-26 09:59:19 +0200

    arduino

--- home/harro/common/core/nixvim/plugins/languages/arduino.nix
+++ home/harro/common/core/nixvim/plugins/languages/arduino.nix
@@ -12,8 +12,21 @@
     home.packages = with pkgs; [
       arduino
       arduino-cli
+      arduino-language-server
+      clang-tools
     ];
 
-    programs.nixvim.plugins.lsp.servers.arduino_language_server.enable = true;
+    programs.nixvim.plugins.lsp.servers.arduino_language_server = {
+      enable = true;
+      cmd = [
+        "arduino-language-server"
+        "-cli"
+        "arduino-cli"
+        "-cli-config"
+        "${config.xdg.configHome}/arduino15/arduino-cli.yaml"
+        "-clangd"
+        "clangd"
+      ];
+    };
   };
 }
