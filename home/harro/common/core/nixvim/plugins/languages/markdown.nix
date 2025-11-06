b6a86575d8b1b85d0768f53bc6b44765dacc36e7
Harro <harro-forge@protonmail.com>
2025-11-06 09:33:04 +0100

    markdown preview

--- home/harro/common/core/nixvim/plugins/languages/markdown.nix
+++ home/harro/common/core/nixvim/plugins/languages/markdown.nix
@@ -9,6 +9,7 @@
   config = lib.mkIf config.nvim.lang.markdown.enable {
     programs.nixvim = {
       plugins.render-markdown.enable = true;
+      plugins.markdown-preview.enable = true;
 
       plugins.lsp.servers.marksman.enable = true;
     };
