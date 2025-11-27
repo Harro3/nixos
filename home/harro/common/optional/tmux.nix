3d45cc2a8134ac700fa8c2c7f6e59c89dc37a183
Harro <harro-forge@protonmail.com>
2025-11-27 16:14:32 +0100

    tmux

--- home/harro/common/optional/tmux.nix
+++ home/harro/common/optional/tmux.nix
@@ -23,7 +23,7 @@
         unbind r
         set-option -sa terminal-overrides ",xterm*:Tc"
         set-option -g default-shell ${pkgs.zsh}/bin/zsh
-        set -g default-terminal "screen-256color"
+        set -g default-terminal "tmux-256color"
 
         set -g prefix C-a
         set -g mouse on
@@ -59,22 +59,21 @@
 
     catppuccin.tmux.enable = true;
     catppuccin.tmux.extraConfig = ''
-      set -g @catppuccin_window_left_separator ""
-      set -g @catppuccin_window_right_separator " "
-      set -g @catppuccin_window_middle_separator " █"
-      set -g @catppuccin_window_number_position "right"
+      set -g status-right-length 100
+      set -g status-left-length 100
+      set -g status-left ""
+      set -g status-right "#{E:@catppuccin_status_application}"
+      set -agF status-right "#{E:@catppuccin_status_cpu}"
+      set -ag status-right "#{E:@catppuccin_status_session}"
+      set -ag status-right "#{E:@catppuccin_status_uptime}"
+      set -agF status-right "#{E:@catppuccin_status_battery}"
 
+      set -g @catppuccin_window_number_position "right"
       set -g @catppuccin_window_default_fill "number"
       set -g @catppuccin_window_default_text "#W"
 
       set -g @catppuccin_window_current_fill "number"
       set -g @catppuccin_window_current_text "#W"
-
-      set -g @catppuccin_status_modules_right "directory user host session"
-      set -g @catppuccin_status_left_separator  " "
-      set -g @catppuccin_status_right_separator ""
-      set -g @catppuccin_status_fill "icon"
-      set -g @catppuccin_status_connect_separator "no"
     '';
   };
 }
